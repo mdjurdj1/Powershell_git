fe30bfedfe0e6b3d6ae7ad076156e257d227e4f4
Michael Djurdjevic <mdjurdj1@gmail.com>
2018-11-26 22:14:17 -0800

    Second commit

--- test.js
+++ test.js
@@ -4,3 +4,12 @@ var y = 2
 var total = x + y
 
 console.log("The total is " + total)
+
+function testArray(array) {
+    while(array.length > 0) {
+        let thing = array.pop()
+        console.log(`Last element of the array is now... ${thing}`)
+    }
+}
+
+testArray([1,2,3,4,5])
