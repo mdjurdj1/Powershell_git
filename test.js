50335a40eaa422cf37ce756c96940dca08b793d0
Michael Djurdjevic <mdjurdj1@gmail.com>
2018-12-02 21:08:10 -0800

    Added the thing

--- test.js
+++ test.js
@@ -6,10 +6,28 @@ var total = x + y
 console.log("The total is " + total)
 
 function testArray(array) {
-    while(array.length > 0) {
+    while (array.length > 0) {
         let thing = array.pop()
         console.log(`Last element of the array is now... ${thing}`)
     }
 }
 
-testArray([1,2,3,4,5])
+function doThing() {
+    alert("You clicked the button.")
+}
+
+testArray([1, 2, 3, 4, 5])
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
